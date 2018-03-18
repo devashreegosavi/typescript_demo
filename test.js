269a557f1d1e58914c6b729ea18a93f9adeb88e0
rails -v <devashree.gosavi@gmail.com>
2018-03-18 16:51:55 +0530

    new

---
+++ test.js
@@ -0,0 +1,14 @@
+// data types
+var isDone = true;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var deci = 1.2;
+var color = "blue";
+color = 'red';
+console.log(color);
+var fullName = "Bob Bobbington";
+var age = 37;
+var sentence = "Hello, my name is " + fullName + ".\nI'll be " + (age + 1) + " years old next month.";
+console.log(sentence);
